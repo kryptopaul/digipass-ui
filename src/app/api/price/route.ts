82070d474bbd07a6a50242f716ee17c3cc56e0c7
kryptopaul <pl4249t@gre.ac.uk>
2023-12-07 20:59:38 +0000

    static price

--- src/app/api/price/route.ts
+++ src/app/api/price/route.ts
@@ -1,7 +1,7 @@
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const destination = searchParams.get("destination");
-  const price = (Math.random() * (10 - 1) + 1).toFixed(2);
-
+  //   const price = (Math.random() * (10 - 1) + 1).toFixed(2);
+  const price = 10.23;
   return Response.json({ destination, price });
 }
