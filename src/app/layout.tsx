91c7e4eb480316a0fa1571b5578ff96caaa4a672
kryptopaul <pl4249t@gre.ac.uk>
2023-12-08 02:18:56 +0000

    connect to avax first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,6 +54,7 @@ export default function RootLayout({
               overlayBlur: "small",
             })}
             chains={chains}
+            initialChain={avalancheFuji}
           >
             {children}{" "}
           </RainbowKitProvider>
